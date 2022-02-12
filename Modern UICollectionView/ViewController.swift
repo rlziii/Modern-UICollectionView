3847be647ed3b79160a9415dbae541c7deeda617
Richard L Zarth III <rlziii@icloud.com>
2022-02-11 22:22:58 -0500

    Initial Commit

---
+++ Modern UICollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Modern UICollectionView
+//
+//  Created by Richard L Zarth III on 2/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
